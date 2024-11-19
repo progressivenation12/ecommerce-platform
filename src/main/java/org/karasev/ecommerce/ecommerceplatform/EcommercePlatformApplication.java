08105cea79ba0016cf6c4457f99da3dc6b774485
Roman Karasev <progressivenation12@gmail.com>
2024-11-19 07:03:52 +0300

    add HealthController

--- src/main/java/org/karasev/ecommerce/ecommerceplatform/EcommercePlatformApplication.java
+++ src/main/java/org/karasev/ecommerce/ecommerceplatform/EcommercePlatformApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EcommercePlatformApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EcommercePlatformApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EcommercePlatformApplication.class, args);
+    }
 
 }
--- src/main/java/org/karasev/ecommerce/ecommerceplatform/controller/HealthController.java
+++ src/main/java/org/karasev/ecommerce/ecommerceplatform/controller/HealthController.java
@@ -1,2 +1,16 @@
-package org.karasev.ecommerce.ecommerceplatform.controller;public class HealtController {
+package org.karasev.ecommerce.ecommerceplatform.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.status(HttpStatus.OK)
+                .body("OK");
+    }
 }
