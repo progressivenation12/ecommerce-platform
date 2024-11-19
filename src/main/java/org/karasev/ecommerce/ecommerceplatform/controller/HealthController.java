ae9221eab3cd0c87613a61104a0e4153ca1ebf4b
Roman Karasev <113823808+progressivenation12@users.noreply.github.com>
2024-11-19 07:15:37 +0300

    Rename HealthController in HealthCheckController

--- src/main/java/org/karasev/ecommerce/ecommerceplatform/controller/HealthController.java
+++ src/main/java/org/karasev/ecommerce/ecommerceplatform/controller/HealthController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HealthController {
+public class HealthCheckController {
 
     @GetMapping("/health")
     public ResponseEntity<String> healthCheck() {
         return ResponseEntity.status(HttpStatus.OK)
-                .body("OK");
+                .body("Application is running");
     }
 }
